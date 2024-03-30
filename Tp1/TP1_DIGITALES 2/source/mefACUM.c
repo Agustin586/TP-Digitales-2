2c9933d2aff573a1d8495c4cef2d3acfe238c1f9
unhexyz <65360044+unhexyz@users.noreply.github.com>
2024-03-29 22:46:36 -0300

    'Completado' mefACUM

--- Tp1/TP1_DIGITALES 2/source/mefACUM.c
+++ Tp1/TP1_DIGITALES 2/source/mefACUM.c
@@ -1,10 +1,125 @@
-/*
- * mefACUM.c
- *
- *  Created on: 29 mar. 2024
- *      Author: aguat
- */
+
+/*==================[inclusions]=============================================*/
+#include "mefACUM.h"
+#include "SD2_board.h"
+#include "key.h"
+
+/*==================[macros and typedef]=====================================*/
+
+typedef enum {
+    EST_ACUM_RESET = 0,
+    EST_ACUM_HPS,
+    EST_ACUM_CS
+}estMefACUM_enum;
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+static estMefACUM_enum estMefACUM;
+static int32_t tim5sACUM;
+static int32_t tim200msACUM;
+static int8_t contAutosACUM = 2;
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+extern void mefACUM_init(void)
+{
+    mefACUM_reset();
+}
+
+extern void mefACUM_reset(void)
+{
+    estMefACUM = EST_ACUM_RESET;
+    
+    tim5sACUM = 300000;
+    tim200msACUM = 200;
+}
 
 
+extern bool mefACUM(void)
+{
+    bool ret = 0;
 
+    switch (estMefSW1)
+    {
+        case EST_ACUM_RESET:
+            
+            if (tim200msACUM == 0)
+            {
+                tim200msACUM = 200;
+                board_setLed(LVR, BOARD_LED_MSG_TOGGLE);
+                board_setLed(LRS, BOARD_LED_MSG_ON);
+                board_setLed(LRR, BOARD_LED_MSG_OFF);
+                board_setLed(LVS, BOARD_LED_MSG_OFF);
+            }
+
+            if( tim5sACUM == 0 )
+            {
+                tim5sACUM = 300000;
+                estMefACUM = EST_ACUM_HPS;
+            }
+
+        break;
+
+        case EST_ACUM_HPS:
+            
+            if( contAutosACUM != 0 )
+            {
+                if( key_getPressEv(SENSOR) ) contAutosACUM--;
+                
+                board_setLed(LRR, BOARD_LED_MSG_ON);
+                board_setLed(LRS, BOARD_LED_MSG_OFF);
+                board_setLed(LVR, BOARD_LED_MSG_OFF);
+                board_setLed(LVS, BOARD_LED_MSG_ON);
+            }
+
+            else
+            {
+                board_setLed(LRR, BOARD_LED_MSG_OFF);
+                board_setLed(LRS, BOARD_LED_MSG_OFF);
+                board_setLed(LVR, BOARD_LED_MSG_OFF);
+                board_setLed(LVS, BOARD_LED_MSG_OFF);
+
+                estMefACUM = EST_ACUM_CS;
+            }
+
+        break;
+
+        case EST_ACUM_CP:
+            
+            if (tim200msACUM == 0)
+            {
+                tim200msACUM = 200;
+                board_setLed(LVS, BOARD_LED_MSG_TOGGLE);
+                board_setLed(LRS, BOARD_LED_MSG_OFF);
+                board_setLed(LVR, BOARD_LED_MSG_OFF);
+                board_setLed(LRR, BOARD_LED_MSG_ON);
+            }
+
+            if (tim5sACUM == 0)
+            {
+                estMefACUM = EST_ACUM_RESET;
+                ret = 1;
+            }
+
+        break;
+
+    }
+
+    return ret;
+}
+
+extern void mefACUM_task1ms(void)
+{
+    if (tim1msACUM)
+        tim1msACUM--;
+}
+
+
+/*==================[end of file]============================================*/
 
--- Tp1/TP1_DIGITALES 2/source/mefACUM.h
+++ Tp1/TP1_DIGITALES 2/source/mefACUM.h
@@ -1,13 +1,28 @@
-/*
- * mefACUM.h
- *
- *  Created on: 29 mar. 2024
- *      Author: aguat
- */
+#ifndef MEF_ACUM_H
+#define MEF_ACUM_H
 
-#ifndef MEFACUM_H_
-#define MEFACUM_H_
+/*==================[inclusions]=============================================*/
+#include "stdbool.h"
 
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+/*==================[macros and typedef]=====================================*/
 
-#endif /* MEFACUM_H_ */
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+extern void mefACUM_reset(void);
+extern void mefACUM_init(void);
+extern bool mefACUM(void);
+extern void mefACUM_task1ms(void);
+
+/*==================[cplusplus]==============================================*/
+#ifdef __cplusplus
+}
+#endif
+
+/*==================[end of file]============================================*/
+#endif /* MEF_ACUM_H */
